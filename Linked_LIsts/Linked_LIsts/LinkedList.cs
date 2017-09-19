a51b0762e07c1230e667129c1ebd44d49742c12f
miyaluc <srlucas3@gmail.com>
2017-09-18 20:09:12 -0700

    finished code challenge

---
+++ Linked_LIsts/Linked_LIsts/LinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_LIsts
+{
+    public class LinkedList
+    {
+        private Node head;
+        private Node current;
+        public int Count;
+
+        public LinkedList()
+        {
+            head = new Node();
+            current = head;
+        }
+
+        public void AddAtLast(object data)
+        {
+            Node newNode = new Node();
+            newNode.Data = data;
+            current.Next = newNode;
+            current = newNode;
+            Count++;
+        }
+        public void AddAtStart(object data)
+        {
+            Node newNode = new Node() { Data = data };
+            newNode.Next = head.Next;
+            head.Next = newNode;
+            Count++;
+        }
+        public void PrintAllNodes()
+        {
+            //Traverse from head
+            Console.Write("Head ->->->->");
+            Node curr = head;
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+                Console.WriteLine(curr.Data);
+                Console.WriteLine("");
+            }
+            Console.Write("->->->->Tail");
+        }
+    }
+}
---
+++ Linked_LIsts/Linked_LIsts/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Linked_LIsts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            LinkedList newList = new LinkedList();
+
+            Console.WriteLine("");
+
+            newList.AddAtLast(24);
+            newList.AddAtLast(42);
+            newList.AddAtLast(6);
+            newList.PrintAllNodes();
+            Console.WriteLine();
+
+            Console.Read();
+        }
+    }
+}
