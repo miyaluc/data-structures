e5d94ea6021ad1eb252aa56810b80c216f09f607
miyaluc <srlucas3@gmail.com>
2017-09-20 17:17:55 -0700

    successfully completed the code challenge

---
+++ nth_element/nth_element/nth_element/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace nth_element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            LinkedList newList = new LinkedList(24);
+            newList.Add(13);
+            newList.Add(15);
+            newList.Add(9);
+            newList.Add(8);
+            newList.Add(28);
+            newList.Add(72);
+            newList.Add(45);
+            newList.Add(3);
+            newList.Add(12);
+
+            newList.PrintMiddle();
+
+            newList.NthFromEnd(4);
+            Console.Read();
+        }
+    }
+}
