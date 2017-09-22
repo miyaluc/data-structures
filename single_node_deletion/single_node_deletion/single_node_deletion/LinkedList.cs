55aa99cd10e8b1f9b27c349e3fc66b2bcdd8004d
miyaluc <srlucas3@gmail.com>
2017-09-21 18:08:02 -0700

    having trouble getting my nodes to display properly

---
+++ single_node_deletion/single_node_deletion/single_node_deletion/LinkedList.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace single_node_deletion
+{
+    class LinkedList
+    {
+        //properties
+        public Node head;
+        public Node curr;
+        public int count = 0;
+        public int midCount;
+        //if remainder is odd, add to midCount
+
+        public LinkedList(Object data)
+        {
+            head = new Node(data);
+            curr = head;
+        }
+        public void Add(Object data)
+        {
+            Node newNode = new Node(data);
+            newNode.Data = data;
+            curr.Next = newNode;
+            curr = newNode;
+            count++;
+            if (count % 2 == 1)
+            {
+                midCount++;
+            }
+        }
+        public void PrintNodes()
+        {
+            Console.Write("Head ->->->");
+            Node curr = head;
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+                Console.WriteLine("curr.Data");
+                Console.WriteLine("");
+            }
+            Console.Write("->->-> Tail");
+        }
+        public void PrintMiddle()
+        {
+            Console.Write("Head ->->->");
+            Console.WriteLine("");
+            int start = 0;
+            Node curr = head;
+            while (start < midCount)
+            {
+                curr = curr.Next;
+                Console.Write(curr.Data);
+                Console.Write("->");
+                start++;
+            }
+            Console.WriteLine("<-<-This is the middle.");
+        }
+        public void SinglyDeleteNode(int input)
+        {
+            int index = input - 1;
+            Console.Write("Head ->");
+            //walker
+            Node curr2 = head;
+            //runner
+            Node curr = head;
+            int innerCount = 0;
+
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+                innerCount++;
+            }
+            for (int i = 0; i < count - index; i++)
+            {
+                curr2 = curr.Next;
+                Console.WriteLine(curr2.Data);
+                Console.Write("->");
+            }
+            curr2 = curr2.Next;
+
+            for (int i = 0; i < index - 1; i++)
+            {
+                curr2 = curr2.Next;
+                Console.WriteLine(curr2.Data);
+                Console.Write("->");
+            }
+            curr2.Next = null;
+
+            Console.WriteLine("This is the new end.");
+        }
+
+    }
+}
+
---
+++ single_node_deletion/single_node_deletion/single_node_deletion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace single_node_deletion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            LinkedList newList = new LinkedList(24);
+            newList.Add(1);
+            newList.Add(2);
+            newList.Add(3);
+            newList.Add(4);
+            newList.Add(5);
+            newList.Add(6);
+            newList.Add(7);
+            newList.Add(8);
+            newList.Add(9);
+
+            newList.PrintNodes();
+            newList.SinglyDeleteNode(3);
+            newList.PrintNodes();
+            Console.Read();
+        }
+
+        //private static void SinglyDeleteNode()
+        //{
+        //    Console.WriteLine("This works!");
+        //    //throw new NotImplementedException();
+        //}
+    }
+}
