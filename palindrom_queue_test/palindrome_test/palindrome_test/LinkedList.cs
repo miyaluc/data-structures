52b6684c73ba0165239866486a462d2617dad89d
miyaluc <srlucas3@gmail.com>
2017-09-28 16:50:24 -0700

    setting up the palindrome test

---
+++ palindrom_queue_test/palindrome_test/palindrome_test/LinkedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace palindrome_test
+{
+    class LinkedList
+    {
+        Node Head;
+        Node Tail;
+
+        void AddChar(char letter)
+        {
+
+        }
+        public bool IsPalindrome()
+        {
+            if(Head == null)
+            {
+                throw new Exception("Check your entry. Letters only.");
+            }
+            runnerForward = Head;
+            runnerBack = Tail;
+            while(runnerForward.value == runnerBack.value)
+            {
+                runnerForward = runnerForward.Next;
+                runnerBack = runnerBack.Previous;
+
+                if(runnerForward.Next == runnerBack || runnerForward == runnerBack.Previous)
+                {
+                    return true;
+                }
+            }
+        }
+        
+    }
+}
---
+++ palindrom_queue_test/palindrome_test/palindrome_test/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace palindrome_test
+{
+    class Node
+    {
+        public int Data
+        {
+            get; set;
+        }
+        public Node Previous
+        {
+            get; set;
+        }
+        public Node Next
+        {
+            get; set;
+        }
+        public Node(int data)
+        {
+            Data = data;
+        }
+    }
+}
