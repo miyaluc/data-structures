7240b65ef166b64d26c970dea323a44a68929338
miyaluc <srlucas3@gmail.com>
2017-09-28 17:30:05 -0700

    added stacks & queues

---
+++ queuewithtwostacks/queuewithtwostacks/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace queuewithtwostacks
+{
+    class Stack
+    {
+        public Node Head
+        {
+            get; set;
+        }
+        public Node Tail
+        {
+            get; set;
+        }
+        public Stack(int Data)
+        {
+            Head = new Node(Data);
+        }
+
+        public Stack()
+        {
+        }
+
+        public void Push(int Data)
+        {
+            Node temp = Head;
+            Head = new Node(Data);
+            Head.Next = temp;
+        }
+        public int Pop()
+        {
+            if(Head == Tail)
+            {
+                Tail = null;
+            }
+            Node temp = Head.Next;
+            Head.Next = null;
+            int value = Head.Data;
+            Head = temp;
+            return value;
+        }
+    }
+}
