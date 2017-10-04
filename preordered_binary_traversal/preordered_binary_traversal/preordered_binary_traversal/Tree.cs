decf6dab9f7a848d4776e0ac70015a9b75703a70
miyaluc <srlucas3@gmail.com>
2017-10-03 17:02:36 -0700

    successfully implemented a binary tree that traverses in a preordered fashion

---
+++ preordered_binary_traversal/preordered_binary_traversal/preordered_binary_traversal/Tree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace preordered_binary_traversal
+{
+    class Tree
+    {
+        public Node Root
+        {
+            get; set;
+        }
+
+        public object Add(object data)
+        {
+            if (Root == null)
+            {
+                Root = new Node(data);
+                return Root.Value;
+            }
+            else
+            {
+                Console.WriteLine("The tree has already been planted.");
+                return null;
+            }
+        }
+        public void Traverse(Node node)
+        {
+            if(node != null)
+            {
+                Console.WriteLine(node.Value);
+                Traverse(node.Left);
+                Traverse(node.Right);
+            }
+        }
+
+        
+    }
+}
