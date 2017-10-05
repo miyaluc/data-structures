6bc490759834815e735a859c78e2171278402e38
miyaluc <srlucas3@gmail.com>
2017-10-04 17:05:57 -0700

    in-order traversal of binary tree complete

---
+++ inorder_binary_traversal/in_order_traversal/in_order_traversal/Tree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace in_order_traversal
+{
+    class Tree
+    {
+        public Node Root
+        {
+            get; set;
+        }
+        public object Add(object data)
+        {
+            if(Root == null)
+            {
+                Root = new Node(data);
+                return Root.Value;
+            }
+            else
+            {
+                Console.WriteLine("The tree has already been planted.");
+                return null;
+            }
+        }
+        public void Traverse(Node node)
+        {
+            if(node != null)
+            {
+                Traverse(node.Left);
+                Console.WriteLine(node.Value);
+                Traverse(node.Right);
+            }
+        }
+    }
+}
