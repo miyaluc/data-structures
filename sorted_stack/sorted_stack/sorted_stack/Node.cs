72b31356bfae76460f55939ed8a9111e9e62623a
miyaluc <srlucas3@gmail.com>
2017-10-16 10:54:17 -0700

    sorted stack

---
+++ sorted_stack/sorted_stack/sorted_stack/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sorted_stack
+{
+    class Node
+    {
+        public int Data
+        {
+            get; set;
+        }
+        public Node Next
+        {
+            get; set;
+        }
+        public Node Prev
+        {
+            get; set;
+        }
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+            Prev = null;
+        }
+    }
+}
---
+++ sorted_stack/sorted_stack/sorted_stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sorted_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here is a sorted stack:");
+            Stack stacky = new Stack(0);
+
+            stacky.Push(7);
+            stacky.Push(2);
+            stacky.Push(5);
+            stacky.Push(3);
+            stacky.Push(8);
+
+            stacky.Print();
+            Console.Read();
+        }
+    }
+}
