2796a04426135349af0d1e7ecfd4814852383760
miyaluc <srlucas3@gmail.com>
2017-10-19 17:13:35 -0700

    challenge complete

---
+++ array_to_balanced_BST/array_to_balanced_BST/array_to_balanced_BST/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace array_to_balanced_BST
+{
+    class Node
+    {
+        public int Data
+        {
+            get; set;
+        }
+        public Node Left
+        {
+            get; set;
+        }
+
+        public Node Right
+        {
+            get; set;
+        }
+
+        public Node(int d)
+        {
+            Data = d;
+            Left = Right = null;
+        }
+    }
+}
---
+++ array_to_balanced_BST/array_to_balanced_BST/array_to_balanced_BST/Tree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace array_to_balanced_BST
+{
+    class Tree
+    {
+
+        static Node root;
+
+        public Node sortedArrayToBST(int[] array, int start, int end)
+        {
+
+            if (start > end)
+            {
+                return null;
+            }
+
+            int mid = (start + end) / 2;
+
+            Node node = new Node(array[mid]);
+
+            node.Left = sortedArrayToBST(array, start, mid - 1);
+
+            node.Right = sortedArrayToBST(array, mid + 1, end);
+
+            return node;
+        }
+        public void preorderedBST(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Console.WriteLine(node.Data + " ");
+            preorderedBST(node.Left);
+            preorderedBST(node.Right);
+        }
+    }
+}
