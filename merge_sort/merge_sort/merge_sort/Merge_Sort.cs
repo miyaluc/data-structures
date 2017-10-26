3d112bc1ce045eed2b4f1d7c8e3d093fe8efc17c
miyaluc <srlucas3@gmail.com>
2017-10-25 20:29:31 -0700

    readme questions answered

---
+++ merge_sort/merge_sort/merge_sort/Merge_Sort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace merge_sort
+{
+    class Merge_Sort
+    {
+        public void DivideDrop(int[] w)
+        {
+            int v = w.Length / 2;
+            int[] newArray1 = new int[v];
+            int[] newArray2 = new int[w.Length];
+
+            for(int i=0; i<v; i++)
+            {
+                newArray1[i] = w[i];
+            }
+            for(int i=v; i<w.Length; i++)
+            {
+                newArray2[i] = w[i];
+            }
+        }
+        public void Merge(int[] x, int[] y)
+        {
+            int[x.Length + y.Length] z;
+            int zIndex = 0;
+            int i = 0;
+            int k = 0;
+
+            for(i; i < x.Length; i++)
+            {
+                for(k; k<y.Length; k++)
+                {
+                    if (y[k] < x[i])
+                    {
+                        z[zIndex] = y[k];
+                    }
+                    else
+                    {
+                        z[zIndex] = x[i];
+                        zIndex++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ radix_sort/radix_sort/radix_sort/Radix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace radix_sort
+{
+    class Radix
+    {
+        public void RadixCount(int n)
+        {
+            int[] array = new int[12];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int p = 10;
+
+                for(int x = 0; x < array.Length;  )
+                {
+                    int n mod p;
+                }
+            }
+        }
+    }
+}
