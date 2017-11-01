96f8889dbf9bd18245c25a9cdc2517f8d5de841d
miyaluc <srlucas3@gmail.com>
2017-10-31 17:30:23 -0700

    setting up an associative array using Blockbuster theme

---
+++ associative_array/AssociativeArrays/AssociativeArrays/Blockbuster.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssociativeArrays
+{
+    class Blockbuster
+    {
+        public string Movie
+        {
+            get; set;
+        }
+        public string Person
+        {
+            get; set;
+        }
+
+        public Blockbuster(string movie, string person)
+        {
+            Movie = movie;
+            Person = person;
+        }
+
+        public string RentMovie()
+        {
+            return Movie;
+        }
+
+        public string GetPerson()
+        {
+            return Person;
+        }
+        public string MoviesOnLoan()
+        {
+            return RentMovie() + "" + GetPerson();
+        }
+
+    }
+}
