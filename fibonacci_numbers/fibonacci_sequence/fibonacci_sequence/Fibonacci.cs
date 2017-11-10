50b6eb7849dbf245732c9085063aa4c026dd55a3
miyaluc <srlucas3@gmail.com>
2017-11-09 17:17:59 -0800

    almost there

---
+++ fibonacci_numbers/fibonacci_sequence/fibonacci_sequence/Fibonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fibonacci_sequence
+{
+    class Fibonacci
+    {
+        public static int Fibby(int n)
+        {
+            //if n is less than or equal to 1
+            if (n <= 1)
+            {
+                //if n is 0 return 0; if n is 1 return 1
+                return n;
+            }
+            //otherwise return recursive call
+            //the Fibonacci formula is Xn = Xn-1 + Xn-2
+            return Fibby(n - 1) + Fibby(n - 2);
+        }
+    }
+}
---
+++ fibonacci_numbers/fibonacci_sequence/fibonacci_sequence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace fibonacci_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Fibonacci FibFab = new Fibonacci();
+
+            Console.WriteLine("Enter a number.");
+            int fibfab = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0; i<fibfab; i++)
+            {
+                Console.WriteLine("{0} ", fibonacci_sequence(i));
+            }
+
+            Console.Read();
+        }
+    }
+
+}
